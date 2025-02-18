a65f2da9d898740ba5b815ab8d4621a761bc69c4
Hans <hansmorillo07@gmail.com>
2025-02-19 00:50:17 +0800

    Added base html files, up until Contact Page. Styling to be done soon.

--- __init__.py
+++ __init__.py
@@ -3,3 +3,50 @@
 # LOADS CONFIGURATIONS
 # IMPORTS AND REGISTERS ROUTES from routes.py
 # RUNS THE FLASK SERVER
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# BEFORE LOGIN ROUTES
+# BEFORE LOGIN ROUTES
+# BEFORE LOGIN ROUTES
+
+@app.route('/')
+def landing():
+    return render_template('loggedout/landing.html')
+
+@app.route('/contactUs')
+def contactUs():
+    return render_template('loggedout/contactUs.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
